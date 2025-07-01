585ef863eb3fdb016ada238e8f71881359a3215a
rittikbasu <irittik@gmail.com>
2025-07-02 02:47:07 +0530

    chore: bump checksums

--- Formula/s33d.rb
+++ Formula/s33d.rb
@@ -6,16 +6,16 @@ class S33d < Formula
     on_macos do
       if Hardware::CPU.intel?
         url "https://github.com/rittikbasu/s33d/releases/download/v0.1.0/s33d-macos-x86_64"
-        sha256 "338299fe7131a76a5c19752c8a4f0415446f2ff0351c6208d94c92830941fb1e"
+        sha256 "7067cdefcdbc01bc0ef854e9b2ccbfc316d155c8fb009b0669bddfa6b8f2a708"
       else
         url "https://github.com/rittikbasu/s33d/releases/download/v0.1.0/s33d-macos-aarch64"
-        sha256 "cef96b1160bd0f16019cc07a233012a3b121aeec08e6dbe51c5470a8d2121d51"
+        sha256 "3a936ad87b6b4d46024f147044275df082955287129c5863cd7a42140f387666"
       end
     end
   
     on_linux do
       url "https://github.com/rittikbasu/s33d/releases/download/v0.1.0/s33d-linux-x86_64"
-      sha256 "57b593ac6f43868230a94c01e7ec591e08b4a3b996485c1c852a356183df21d2"
+      sha256 "8155eb28ba4425c7af4e7a5e862414d8ab5057966b13089bfb84f1a65ade6398"
     end
   
     def install
